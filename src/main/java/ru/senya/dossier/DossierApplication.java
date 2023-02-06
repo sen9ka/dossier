f7d0956d4c09e1606f00e6148a9af647dffbed11
sen9ka <ars-shvec@yandex.ru>
2023-02-06 17:28:43 +0300

    Initial commit

---
+++ src/main/java/ru/senya/dossier/DossierApplication.java
@@ -0,0 +1,15 @@
+package ru.senya.dossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class DossierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DossierApplication.class, args);
+	}
+
+}
